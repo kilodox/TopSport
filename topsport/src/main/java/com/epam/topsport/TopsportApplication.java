b1c17bbf43e34fb6002f10f5dda7a136f06baef0
Stoyan Georgiev <kilodox@abv.bg>
2019-06-12 16:49:32 +0300

    first steps

---
+++ topsport/src/main/java/com/epam/topsport/TopsportApplication.java
@@ -0,0 +1,13 @@
+package com.epam.topsport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopsportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopsportApplication.class, args);
+	}
+
+}
